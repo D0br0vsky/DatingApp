a155c23380f24abe88a6e68f1e8dfd0f67bdfd05
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-08-03 10:37:21 +0300

    Initial Commit

---
+++ DatingApp/DatingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DatingAppApp.swift
+//  DatingApp
+//
+//  Created by Dobrovsky on 03.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct DatingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
