7a95f3a304560c2446bd261582f41a8c4e4c8355
Dobrovsky <miron.dobrovsky.work@gmail.com>
2024-08-16 20:35:29 +0300

    Redesigned the partner search mode and the main screen format

---
+++ DatingApp/Views/Components/ProfilePhotoChooseView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfilePhotoChuseView.swift
+//  DatingApp
+//
+//  Created by Dobrovsky on 14.08.2024.
+//
+
+import SwiftUI
+
+struct ProfilePhotoChooseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfilePhotoChooseView()
+}
